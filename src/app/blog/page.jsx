f6cca0f6ed984342fcc6a4b1faf87a473fd356c6
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-02 13:54:57 +0530

    added single blog css and blog page layout

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,15 @@
+import PostCard from '@/components/postCard/postCard'
+import styles from './blog.module.css'
 import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className={styles.container}>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+    </div>
   )
 }
 
