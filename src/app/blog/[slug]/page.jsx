a27a50899dc7cd585176c541f4d02e0d64608f70
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-02 15:36:22 +0530

    added post detail layout

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from './singlePost.module.css'
+import Image from 'next/image'
+
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+          <Image src='/post.png' alt="" fill className={styles.img} />
+        </div>
+      <div className={styles.textContainer}>
+      <h1 className={styles.title}>Title</h1>
+      <div className={styles.detail}>
+       <Image src='/user.png' alt="" className={styles.avatar} width={50} height={50}/>
+       <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Name</span></div>
+        <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>Date</span>
+       </div>
+      </div>
+      <div className={styles.content}>Desc</div>
+      </div>
+    </div>
   )
 }
 
