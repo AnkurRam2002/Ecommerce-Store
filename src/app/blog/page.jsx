d12d53cc241999d7590d8ac06ad9007c36cd709e
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-04 11:20:58 +0530

    added mock json server to test data fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,23 +2,25 @@ import PostCard from '@/components/postCard/postCard'
 import styles from './blog.module.css'
 import React from 'react'
 
-const BlogPage = () => {
+const getData = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await res.json()
+  return data
+}
+
+const BlogPage = async () => {
+
+  const posts = await getData()
+
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default BlogPage
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -13,8 +13,8 @@ const PostCard = ({post}) => {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>Desc</p>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
         <Link className={styles.link} href={`/blog/post`}>READ MORE</Link>
       </div>
     </div>
