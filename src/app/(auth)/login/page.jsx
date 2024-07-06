62286e57e012568e367f61e33ebe296da359b422
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-06 15:36:57 +0530

    added login form component and css

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,18 +1,16 @@
-//import LoginForm from "@/components/loginForm/loginForm";
+import LoginForm from "@/components/loginForm/loginForm";
 import { handleGithubLogin } from "@/lib/action";
 import styles from "./login.module.css";
-import { auth, signIn } from "@/lib/auth";
 
-const LoginPage = async() => {
-  const session = await auth();
+const LoginPage = () => {
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
         <form action={handleGithubLogin}>
-          <button>Login with Github</button>
+          <button className={styles.github}>Login with Github</button>
         </form>
-
+        <LoginForm />
       </div>
     </div>
   );
