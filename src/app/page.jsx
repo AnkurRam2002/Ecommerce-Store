7aa0c3e584e24182ab2bbf501e223025fff2a6f9
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-25 21:43:41 +0530

    blog changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,8 +9,7 @@ const Home = () => {
     <div className={styles.textContainer}>
       <h1 className={styles.title}>Explore the World Through Stories</h1>
       <p className={styles.desc}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
-        blanditiis adipisci minima reiciendis a autem assumenda dolore.
+      Your ultimate destination for captivating travel stories and adventures. Immerse yourself in tales from around the world, discover hidden gems, and find inspiration for your next journey. Join our community of explorers and share your unique travel experiences. Whether you’re a seasoned globetrotter or planning your first trip, Wanderlust Chronicles has something for everyone. Let’s embark on this journey together!
       </p>
       <div className={styles.buttons}>
         <Link href={'./blog'}><button className={styles.button}>Read Stories</button></Link>
